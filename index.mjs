5675026b7a7d0329ddd586ac353166eaa0f62178
Eugene Song <tilleps@gmail.com>
2023-05-05 05:41:29 -0700

    Add package namespace import example

--- index.mjs
+++ index.mjs
@@ -1,10 +1,10 @@
 import config from "./config/index.mjs";
 
+import { fullName } from "jsdoc-typescript-example/name";
 
 /**
  * @typedef {import("./config/index.mjs").default} Config
  */
-
 
 /**
  *
@@ -14,3 +14,7 @@ function doSomethingWithConfig(config) {
   console.log(config.port);
 }
 
+
+let name = fullName("John", "Doe");
+
+console.log(name);
